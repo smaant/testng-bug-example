1d15164eeac89040bd51914f817af5f70b103ceb
Anton Panferov <a.panfyorov@hh.ru>
2013-12-16 19:01:47 +0400

    Initial commit

---
+++ src/test/java/com/mycompany/app/with_inheritance/WithClass1.java
@@ -0,0 +1,37 @@
+package com.mycompany.app.with_inheritance;
+
+import org.junit.Assert;
+import org.testng.annotations.*;
+
+public class WithClass1 extends SuperClass {
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println(this.getClass().getName() + ": before class\n");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println(this.getClass().getName() + ": after class\n\n");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println(this.getClass().getName() + ": before method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println(this.getClass().getName() + ": after method\n");
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(this.getClass().getName() + ": test #1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(this.getClass().getName() + ": test #2");
+    }
+}
---
+++ src/test/java/com/mycompany/app/with_inheritance/WithClass2.java
@@ -0,0 +1,35 @@
+package com.mycompany.app.with_inheritance;
+
+import org.testng.annotations.*;
+
+public class WithClass2 extends SuperClass {
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println(this.getClass().getName() + ": before class\n");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println(this.getClass().getName() + ": after class\n\n");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println(this.getClass().getName() + ": before method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println(this.getClass().getName() + ": after method\n");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(this.getClass().getName() + ": test #1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(this.getClass().getName() + ": test #2");
+    }
+}
